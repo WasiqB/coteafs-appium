2f830215ac78c2ff78ee113273488126f866d96d
WasiqB <wasbhamla2005@gmail.com>
2018-02-01 16:39:15 +0530

    Swipe related changes is in progress.

---
+++ src/test/java/com/github/wasiqb/coteafs/appium/android/vodqa/activities/SliderActivity.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2017, Wasiq Bhamla.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.wasiqb.coteafs.appium.android.vodqa.activities;
+
+import org.openqa.selenium.By;
+
+import com.github.wasiqb.coteafs.appium.android.AndroidDevice;
+import com.github.wasiqb.coteafs.appium.device.DeviceElement;
+import com.github.wasiqb.coteafs.appium.device.WaitStrategy;
+
+/**
+ * @author wasiq.bhamla
+ * @since Feb 1, 2018 3:02:34 PM
+ */
+public class SliderActivity extends DefaultActivity {
+	/**
+	 * @author wasiq.bhamla
+	 * @since Feb 1, 2018 3:02:34 PM
+	 * @param device
+	 */
+	public SliderActivity (final AndroidDevice device) {
+		super (device);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.github.wasiqb.coteafs.appium.android.vodqa.activities.DefaultActivity#prepare()
+	 */
+	@Override
+	protected DeviceElement prepare () {
+		final DeviceElement main = super.prepare ();
+
+		DeviceElement.create ("Slider")
+			.parent (main)
+			.waitStrategy (WaitStrategy.ENABLED)
+			.using (By.id ("slider"));
+
+		return main;
+	}
+}
