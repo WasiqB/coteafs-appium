b31304722b7489ac2af01018bd6a491b0c044f3b
WasiqB <wasbhamla2005@gmail.com>
2018-09-19 21:54:06 +0530

    Updated test class for compilation error in CircleCI.

--- src/test/java/com/github/wasiqb/coteafs/appium/android/VodQATest.java
+++ src/test/java/com/github/wasiqb/coteafs/appium/android/VodQATest.java
@@ -15,9 +15,6 @@
  */
 package com.github.wasiqb.coteafs.appium.android;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -30,11 +27,6 @@ import com.github.wasiqb.coteafs.appium.android.vodqa.activities.VerticleSwipeAc
 import com.github.wasiqb.coteafs.appium.device.SwipeDirection;
 import com.github.wasiqb.coteafs.appium.device.SwipeStartPosition;
 
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.MultiTouchAction;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-
 /**
  * @author wasiq.bhamla
  * @since Jan 23, 2018 8:14:51 PM
@@ -42,60 +34,19 @@ import io.appium.java_client.android.AndroidDriver;
 public class VodQATest extends DefaultTest {
 	/**
 	 * @author wasiq.bhamla
-	 * @since Feb 2, 2018 9:56:28 PM
-	 */
-	@Test (enabled = false)
-	public void test1 () {
-		this.main.onElement ("PhotoView")
-			.click ();
-
-		final AndroidDriver <MobileElement> driver = this.androidDevice.getDriver ();
-		final MobileElement img = driver.findElement (By.className ("android.widget.ImageView"));
-
-		final Point location = img.getLocation ();
-		final Dimension size = img.getSize ();
-		final int x1 = location.getX ();
-		final int y1 = location.getY ();
-
-		System.out.println ("x is " + x1 + "y1 is " + y1);
-
-		final int x = size.getWidth () / 2;
-		final int y = size.getHeight () / 2;
-
-		img.click ();
-
-		// Zoom
-		System.out.println ("Now zooming");
-		final TouchAction finger1 = new TouchAction (driver);
-		finger1.press (img, x, y - 10)
-			.moveTo (img, x, y - 400);
-
-		final TouchAction finger2 = new TouchAction (driver);
-		finger2.press (img, x, y + 10)
-			.moveTo (img, x, y + 400);
-
-		final MultiTouchAction action = new MultiTouchAction (driver);
-		action.add (finger1)
-			.add (finger2)
-			.perform ();
-
-	}
-
-	/**
-	 * @author wasiq.bhamla
 	 * @since Feb 2, 2018 2:59:25 PM
 	 */
 	@Test
 	public void testDragDrop () {
 		this.main.onElement ("DragDrop")
-			.click ();
+				.click ();
 
 		final DragDropActivity dd = new DragDropActivity (this.androidDevice);
 		dd.onElement ("DropMe")
-			.dragDrop (dd.getElement ("DropZone"));
+				.dragDrop (dd.getElement ("DropZone"));
 		dd.onElement ("Success")
-			.verifyThat ()
-			.textShouldBeEqualTo ("Circle dropped");
+				.verifyThat ()
+				.textShouldBeEqualTo ("Circle dropped");
 	}
 
 	/**
@@ -105,13 +56,13 @@ public class VodQATest extends DefaultTest {
 	@Test
 	public void testLongPress () {
 		this.main.onElement ("LongPress")
-			.click ();
+				.click ();
 
 		final LongPressActivity lp = new LongPressActivity (this.androidDevice);
 		lp.onElement ("Button")
-			.longPress ();
+				.longPress ();
 		final String message = lp.onDevice ()
-			.handleAlert ();
+				.handleAlert ();
 		Assert.assertEquals (message, "you pressed me hard :P");
 	}
 
@@ -123,18 +74,18 @@ public class VodQATest extends DefaultTest {
 	@Test
 	public void testNativeView () throws InterruptedException {
 		this.main.onElement ("ChainedView")
-			.click ();
+				.click ();
 
 		final ChainedViewActivity chained = new ChainedViewActivity (this.androidDevice);
 		chained.onElement ("Text1")
-			.verifyThat ()
-			.textShouldBeEqualTo ("Hello World, I'm View one ");
+				.verifyThat ()
+				.textShouldBeEqualTo ("Hello World, I'm View one ");
 		chained.onElement ("Text2")
-			.verifyThat ()
-			.textShouldBeEqualTo ("Hello World, I'm View two ");
+				.verifyThat ()
+				.textShouldBeEqualTo ("Hello World, I'm View two ");
 		chained.onElement ("Text3")
-			.verifyThat ()
-			.textShouldBeEqualTo ("Hello World, I'm View three ");
+				.verifyThat ()
+				.textShouldBeEqualTo ("Hello World, I'm View three ");
 	}
 
 	/**
@@ -144,13 +95,13 @@ public class VodQATest extends DefaultTest {
 	@Test
 	public void testSlider () {
 		this.main.onElement ("Slider")
-			.click ();
+				.click ();
 
 		final SliderActivity slide = new SliderActivity (this.androidDevice);
 		slide.onElement ("Slider")
-			.swipe (SwipeDirection.RIGHT, SwipeStartPosition.LEFT, 25);
+				.swipe (SwipeDirection.RIGHT, SwipeStartPosition.LEFT, 25);
 		slide.onElement ("Slider1")
-			.swipe (SwipeDirection.LEFT, SwipeStartPosition.RIGHT, 25);
+				.swipe (SwipeDirection.LEFT, SwipeStartPosition.RIGHT, 25);
 	}
 
 	/**
@@ -160,13 +111,13 @@ public class VodQATest extends DefaultTest {
 	@Test
 	public void testVerticleSwipe () {
 		this.main.onElement ("VerticalSwipe")
-			.click ();
+				.click ();
 
 		final VerticleSwipeActivity vs = new VerticleSwipeActivity (this.androidDevice);
 		vs.onElement ("List")
-			.swipe (SwipeDirection.UP, SwipeStartPosition.BOTTOM, 25);
+				.swipe (SwipeDirection.UP, SwipeStartPosition.BOTTOM, 25);
 		vs.onElement ("List")
-			.swipe (SwipeDirection.DOWN, SwipeStartPosition.TOP, 25);
+				.swipe (SwipeDirection.DOWN, SwipeStartPosition.TOP, 25);
 	}
 
 	/**
@@ -176,13 +127,13 @@ public class VodQATest extends DefaultTest {
 	@Test
 	public void testZoomPinch () {
 		this.main.onElement ("PhotoView")
-			.click ();
+				.click ();
 
 		final PhotoViewActivity p = new PhotoViewActivity (this.androidDevice);
 		p.onElement ("Img")
-			.zoom (25);
+				.zoom (25);
 
 		p.onElement ("Img")
-			.pinch (25);
+				.pinch (25);
 	}
 }
